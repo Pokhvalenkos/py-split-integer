ed62beabb035e94f2c0f45e07a83e45bc0716c2e
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-09-08 14:12:52 +0200

    Resolved problems with flake8

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,30 +2,30 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert(
+    assert (
         sum(split_integer(17, 4)) == 17
     ), "Sum of result parts should be equal to value"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert(
+    assert (
         set(split_integer(30, 5)) == {6}
     ), "Should split into equal parts when value divisible by parts"
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert(
+    assert (
         split_integer(123, 1) == [123]
     ), "Should return part equals to value when split into one part"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert(
+    assert (
         split_integer(17, 3) == [5, 6, 6]
     ), "Parts should be sorted when they are not equal"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert(
+    assert (
         split_integer(3, 5) == [0, 0, 1, 1, 1]
     ), "Should add zeros when value is less than number of parts"
